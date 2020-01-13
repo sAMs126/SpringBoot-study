6f3e7fb6657237c6bcf82a2840b511eb534ca1e8
sAMs126 <492457689@qq.com>
2020-01-13 19:45:02 +0800

    :bulb: 根据运行环境切换不同的配置文件

---
+++ SpringBoot-03-Profile/src/main/java/com/example/controller/Example.java
@@ -0,0 +1,20 @@
+package com.example.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * A Controller Example
+ *
+ * @author sAMs126
+ * @date 2020/1/13
+ */
+@RestController
+public class Example {
+
+	@RequestMapping("/hello")
+	public String hello() {
+		return "Hello World!";
+	}
+}
